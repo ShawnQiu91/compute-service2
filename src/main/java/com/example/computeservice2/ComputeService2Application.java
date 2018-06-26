e790b53a2b7582458c0a7de4e209cea46a2fdedc
TimQiu91 <123040844@qq.com>
2018-06-26 18:02:40 +0800

    first commit

---
+++ src/main/java/com/example/computeservice2/ComputeService2Application.java
@@ -0,0 +1,14 @@
+package com.example.computeservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ComputeService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputeService2Application.class, args);
+	}
+}
